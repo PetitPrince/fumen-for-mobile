7661f407b16544eb2615950b1dfeba9e2978e774
PetitPrince <code@hoangpham.ch>
2025-01-30 23:58:36 +0100

    Empty

--- resources/jump.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){encode(1);window.open('https://knewjade.github.io/fumen-for-mobile/index.html?d='+encodeURIComponent(document.getElementById('tx').value), '_blank');})();
--- src/env.ts
+++
@@ -1,4 +0,0 @@
-export const PageEnv = {
-    Version: '###VERSION###',
-    Debug: JSON.parse('###DEBUG###') as boolean,
-};
